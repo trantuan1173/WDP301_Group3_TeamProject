ba0f4a68d4b64391512c800fd65f5da9ee60d2c0
TienBV <buivutien1508@gmail.com>
2025-05-16 10:43:11 +0700

    Update Frontend Temaplate

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        WDP301_Project
+      </h1>
+    </div>
+  )
+}
+
+export default App
