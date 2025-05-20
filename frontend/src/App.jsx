acbc658bb910a3b3d3255d6b1976251145554fc1
TienBV <buivutien1508@gmail.com>
2025-05-20 09:53:04 +0700

    sa

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,15 @@
 import React from 'react'
-<<<<<<< Updated upstream
-=======
+
 
 import AdminDashboard from './pages/Dashboard/AdminDashboard'
->>>>>>> Stashed changes
+
 
 const App = () => {
   return (
     <div>
-<<<<<<< Updated upstream
-      <h1 className="text-3xl font-bold underline">
-        WDP301_Project
-      </h1>
-=======
+
       <AdminDashboard/>
->>>>>>> Stashed changes
+
     </div>
   )
 }
