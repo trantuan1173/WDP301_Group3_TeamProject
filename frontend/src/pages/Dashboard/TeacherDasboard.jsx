fc468ca7af1a2b4d3decc1903fd8b64e645d0832
TienBV <buivutien1508@gmail.com>
2025-06-03 09:26:50 +0700

    Update teacher silde css

--- frontend/src/pages/Dashboard/TeacherDasboard.jsx
+++ frontend/src/pages/Dashboard/TeacherDasboard.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import TeacherSideMenu from "../../components/Layouts/TeacherSideMenu";
+import NavBar from "../../components/Layouts/NavBar";
 
 const TeacherDashboard = () => {
   return (
+    <div className="h-screen flex flex-col">
+          <header className="w-full">
+            <NavBar />
+          </header>
+         
     <div className="flex min-h-screen bg-gray-50">
       <TeacherSideMenu />
-      <div className="flex-1 p-8">
+      <div className="flex-1 p-4">
         {/* Nội dung dashboard ở đây */}
         <h1 className="text-2xl font-bold mb-4">Teacher Dashboard</h1>
       </div>
     </div>
+  </div>
   );
 };
 
