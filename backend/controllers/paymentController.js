6f69846bbc4a8065add6aef2fcd98490f8112afe
Tran Anh Tuan <trantuan1172003@gmail.com>
2025-06-13 14:19:10 +0700

    Revert "Update paymentController.js"
    This reverts commit 55bc8bb9cd2e78821535efaf857b6241720a55dd.

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -582,15 +582,18 @@ const refund = (req, res) => {
 };
 
 function sortObject(obj) {
-  const sorted = {};
-  const keys = Object.keys(obj).sort();
-
-  for (const key of keys) {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) {
-      sorted[key] = obj[key];
+  let sorted = {};
+  let str = [];
+  let key;
+  for (key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      str.push(encodeURIComponent(key));
     }
   }
-
+  str.sort();
+  for (key = 0; key < str.length; key++) {
+    sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+");
+  }
   return sorted;
 }
 
