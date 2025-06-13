19b9420c1d32a817f953ad4c05f52152af7ef356
Tran Anh Tuan <trantuan1172003@gmail.com>
2025-06-13 22:15:45 +0700

    Update paymentController.js

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -491,13 +491,51 @@ const vnpayIpn = async (req, res) => {
   console.log("Generated Signed Hash:", signed);
 
   // 7. So sánh hash
-  if (secureHash === signed) {
-    // TODO: Kiểm tra vnp_TxnRef, vnp_Amount, trạng thái đơn hàng trong DB ở đây
+  // if (secureHash === signed) {
+  //   // TODO: Kiểm tra vnp_TxnRef, vnp_Amount, trạng thái đơn hàng trong DB ở đây
 
-    // Giả sử đơn hợp lệ
-    res.status(200).json({ RspCode: '00', Message: 'Success' });
-  } else {
-    res.status(200).json({ RspCode: '97', Message: 'Checksum failed' });
+  //   // Giả sử đơn hợp lệ
+  //   res.status(200).json({ RspCode: '00', Message: 'Success' });
+  // } else {
+  //   res.status(200).json({ RspCode: '97', Message: 'Checksum failed' });
+  // }
+  let paymentStatus = '0'; // Giả sử '0' là trạng thái khởi tạo giao dịch, chưa có IPN. Trạng thái này được lưu khi yêu cầu thanh toán chuyển hướng sang Cổng thanh toán VNPAY tại đầu khởi tạo đơn hàng.
+  //let paymentStatus = '1'; // Giả sử '1' là trạng thái thành công bạn cập nhật sau IPN được gọi và trả kết quả về nó
+  //let paymentStatus = '2'; // Giả sử '2' là trạng thái thất bại bạn cập nhật sau IPN được gọi và trả kết quả về nó
+  
+  let checkOrderId = true; // Mã đơn hàng "giá trị của vnp_TxnRef" VNPAY phản hồi tồn tại trong CSDL của bạn
+  let checkAmount = true; // Kiểm tra số tiền "giá trị của vnp_Amout/100" trùng khớp với số tiền của đơn hàng trong CSDL của bạn
+  if(secureHash === signed){ //kiểm tra checksum
+      if(checkOrderId){
+          if(checkAmount){
+              if(paymentStatus=="0"){ //kiểm tra tình trạng giao dịch trước khi cập nhật tình trạng thanh toán
+                  if(rspCode=="00"){
+                      //thanh cong
+                      //paymentStatus = '1'
+                      // Ở đây cập nhật trạng thái giao dịch thanh toán thành công vào CSDL của bạn
+                      res.status(200).json({RspCode: '00', Message: 'Success'})
+                  }
+                  else {
+                      //that bai
+                      //paymentStatus = '2'
+                      // Ở đây cập nhật trạng thái giao dịch thanh toán thất bại vào CSDL của bạn
+                      res.status(200).json({RspCode: '00', Message: 'Success'})
+                  }
+              }
+              else{
+                  res.status(200).json({RspCode: '02', Message: 'This order has been updated to the payment status'})
+              }
+          }
+          else{
+              res.status(200).json({RspCode: '04', Message: 'Amount invalid'})
+          }
+      }       
+      else {
+          res.status(200).json({RspCode: '01', Message: 'Order not found'})
+      }
+  }
+  else {
+      res.status(200).json({RspCode: '97', Message: 'Checksum failed'})
   }
 };
 
