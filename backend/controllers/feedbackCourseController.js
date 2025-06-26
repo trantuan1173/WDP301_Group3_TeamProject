cf082870c431f945d49406faef8923b2bb752208
Tran Anh Tuan <trantuan1172003@gmail.com>
2025-06-27 00:37:02 +0700

    Done Feedback

---
+++ backend/controllers/feedbackCourseController.js
@@ -0,0 +1,179 @@
+const express = require("express");
+const router = express.Router();
+const FeedbackCourse = require("../models/feedbackCourseModel.js");
+const User = require("../models/userModel.js");
+const Course = require("../models/courseModel.js");
+
+// Get all feedbacks (Admin)
+const getAllFeedbacksAllCourse = async (req, res) => {
+  try {
+    const feedbacks = await FeedbackCourse.find()
+    res.status(200).send({
+      success: true,
+      count: feedbacks.length,
+      data: feedbacks,
+    })
+    message: "Feedbacks fetched successfully";
+  } catch (error) {
+    res.status(500).send(error)
+    message: "Failed to fetch feedbacks";
+  }
+}
+
+//Get feedbacks by course (User)
+const getAllFeedbacksACourse = async (req, res) => {
+  try {
+    const { courseId } = req.params
+    const feedbacks = await FeedbackCourse.find({ courseId })
+    res.status(200).send({
+      success: true,
+      count: feedbacks.length,
+      data: feedbacks,
+    })
+    message: "Feedbacks fetched successfully";
+  } catch (error) {
+    res.status(500).send(error)
+    message: "Failed to fetch feedbacks";
+  }
+}
+
+// Get feedbacks of user (Admin)
+const getAllFeedbacksCourseByUser = async (req, res) => {
+  try {
+    const { userId } = req.params
+    const feedbacks = await FeedbackCourse.find({ userId })
+    res.status(200).send({
+      success: true,
+      count: feedbacks.length,
+      data: feedbacks,
+    })
+    message: "Feedbacks fetched successfully";
+  } catch (error) {
+    res.status(500).send(error)
+    message: "Failed to fetch feedbacks";
+  }
+}
+
+// Get single feedback
+const getFeedbackCourseHighlight = async (req, res) => {
+  try {
+    const { courseId } = req.params
+    const feedback = await FeedbackCourse.find({courseId, highlight: true})
+    if (!feedback) {
+      return res.status(404).send("Feedback not found");
+    }
+    res.status(200).send({
+      success: true,
+      data: feedback,
+    })
+    message: "Feedback fetched successfully";
+  } catch (error) {
+    res.status(500).send(error)
+    message: "Failed to fetch feedback";
+  }
+}
+
+// Create feedback
+const createFeedbackCourse = async (req, res) => {
+  try {
+    const { userId, courseId, feedback, rating } = req.body;
+    const user = await User.findById(userId);
+    const course = await Course.findById(courseId);
+    if (!user || !course) {
+      return res.status(404).send("User or course not found");
+    }
+    const feedbackCourse = new FeedbackCourse({
+      userId,
+      courseId,
+      feedback,
+      rating,
+    });
+    await feedbackCourse.save();
+    res.status(201).send({
+      success: true,
+      data: feedbackCourse,
+    })
+    message: "Feedback created successfully";
+  } catch (error) {
+    res.status(400).send(error);
+    message: "Failed to create feedback";
+  }
+}
+
+// Update feedback
+const updateFeedbackCourse = async (req, res) => {
+  try {
+    const feedback = await FeedbackCourse.findOneAndUpdate(
+      {_id: req.params.id}, 
+      {...req.body},
+      {
+        new: true,
+        runValidators: true,
+      },
+    );
+    if (!feedback) {
+      return res.status(404).send("Feedback not found");
+    }
+    res.status(200).send({
+      success: true,
+      data: feedback,
+    })
+    message: "Feedback updated successfully";
+  } catch (error) {
+    res.status(400).send(error);
+    message: "Failed to update feedback";
+  }
+}
+
+// Delete feedback
+const deleteFeedbackCourse = async (req, res) => {
+  try {
+    const { id } = req.params
+    const feedback = await FeedbackCourse.findByIdAndDelete({_id: id});
+    if (!feedback) {
+      return res.status(404).send("Feedback not found");
+    }
+    res.status(200).send({
+      success: true,
+      data: feedback,
+    })
+    message: "Feedback deleted successfully";
+  } catch (error) {
+    res.status(500).send(error);
+    message: "Failed to delete feedback";
+  }
+}
+
+// Highlight feedback for home page by admin
+const highlightFeedbackCourse = async (req, res) => {
+  try {
+    const feedback = await FeedbackCourse.findById(req.params.id)
+    const highlight = req.body.highlight
+    if (!feedback) {
+      return res.status(404).send("Feedback not found");
+    }
+
+    feedback.highlight = highlight;
+    await feedback.save();
+    res.status(200).send({
+      success: true,
+      data: feedback,
+      message: "Feedback highlighted successfully",
+    })
+  } catch (error) {
+    res.status(500).send(error);
+    message: "Failed to highlight feedback";
+  }
+}
+
+
+module.exports = {
+  getAllFeedbacksAllCourse,
+  getAllFeedbacksACourse,
+  getAllFeedbacksCourseByUser,
+  getFeedbackCourseHighlight,
+  createFeedbackCourse,
+  updateFeedbackCourse,
+  deleteFeedbackCourse,
+  highlightFeedbackCourse,
+};
---
+++ backend/routes/feedbackTeacherRoutes.js
@@ -0,0 +1,210 @@
+const express = require("express")
+const {
+  getAllFeedbacksAllTeacher,
+  getAllFeedbacksATeacher,
+  getFeedbacksTeacherByUser,
+  getFeedbacksTeacherHighlight,
+  createFeedbackTeacher,
+  updateFeedbackTeacher,
+  deleteFeedbackTeacher,
+  highlightFeedbackTeacher
+} = require("../controllers/feedbackTeacherController.js")
+const { protect, authorize } = require("../middleware/authMiddleware.js")
+
+const router = express.Router()
+
+/**
+ * @swagger
+ * tags:
+ *   name: Feedbacks Teacher
+ *   description: Phản hồi
+ */
+
+/**
+ * @swagger
+ * /feedbacksTeacher:
+ *   get:
+ *     summary: Lấy danh sách phản hồi (chỉ admin)
+ *     description: Lấy danh sách phản hồi (chỉ admin)
+ *     tags: [Feedbacks Teacher]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Danh sách phản hồi
+ */
+router.get("/", protect, authorize("admin"), getAllFeedbacksAllTeacher)
+
+/**
+ * @swagger
+ * /feedbacksTeacher:
+ *   post:
+ *     summary: Tạo phản hồi
+ *     description: Tạo phản hồi
+ *     tags: [Feedbacks Teacher]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               courseId:
+ *                 type: string
+ *               studentId:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Tạo phản hồi thành công
+ */
+router.post("/", protect, createFeedbackTeacher)
+
+/**
+ * @swagger
+ * /feedbacksTeacher/highlight/:id:
+ *   get:
+ *     summary: Lấy thông tin phản hồi highlight
+ *     description: Lấy thông tin phản hồi highlight
+ *     tags: [Feedbacks Teacher]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Thông tin phản hồi
+ */
+router.get("/highlight/:teacherId", getFeedbacksTeacherHighlight)
+
+/**
+ * @swagger
+ * /feedbacksTeacher/user/:studentId:
+ *   get:
+ *     summary: Lấy danh sách phản hồi của người dùng (chỉ admin)
+ *     description: Lấy danh sách phản hồi của người dùng (chỉ admin)
+ *     tags: [Feedbacks Teacher]
+ *     parameters:
+ *       - in: path
+ *         name: studentId
+ *         required: true
+ *         schema:
+ *           type: string
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Danh sách phản hồi
+ */
+router.get("/user/:studentId", protect, authorize("admin"), getFeedbacksTeacherByUser)
+
+/**
+ * @swagger
+ * /feedbacksTeacher/:id:
+ *   put:
+ *     summary: Cập nhật thông tin phản hồi
+ *     description: Cập nhật thông tin phản hồi
+ *     tags: [Feedbacks Teacher]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               courseId:
+ *                 type: string
+ *               studentId:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Cập nhật thông tin phản hồi thành công
+ */
+router.put("/:id", protect, updateFeedbackTeacher)
+
+/**
+ * @swagger
+ * /feedbacksTeacher/:id:
+ *   delete:
+ *     summary: Xóa phản hồi (chỉ admin)
+ *     description: Xóa phản hồi (chỉ admin)
+ *     tags: [Feedbacks Teacher]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Xóa phản hồi thành công
+ */
+router.delete("/:id", protect, authorize("admin"), deleteFeedbackTeacher)
+
+/**
+ * @swagger
+ * /feedbacksTeacher/highlight/:id:
+ *   put:
+ *     summary: Cập nhật thông tin phản hồi (chỉ admin)
+ *     description: Cập nhật thông tin phản hồi (chỉ admin)
+ *     tags: [Feedbacks Teacher]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               highlight:
+ *                 type: boolean
+ *     responses:
+ *       200:
+ *         description: Cập nhật thông tin phản hồi thành công
+ */
+router.put("/highlight/:id", protect, authorize("admin"), highlightFeedbackTeacher)
+
+/**
+ * @swagger
+ * /feedbacksTeacher/:teacherId:
+ *   get:
+ *     summary: Lấy danh sách phản hồi theo giáo viên
+ *     description: Lấy danh sách phản hồi theo giáo viên
+ *     tags: [Feedbacks Teacher]
+ *     parameters:
+ *       - in: path
+ *         name: teacherId
+ *         required: true
+ *         schema:
+ *           type: string
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Danh sách phản hồi
+ */
+router.get("/:teacherId", protect, getAllFeedbacksATeacher)
+
+module.exports = router
