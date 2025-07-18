977351b5c7869dab3adc6bcf7c6e06c5d7fa2202
Tran Anh Tuan <trantuan1172003@gmail.com>
2025-07-18 19:37:23 +0700

    Update RequireAuth.jsx

--- frontend/src/context/RequireAuth.jsx
+++ frontend/src/context/RequireAuth.jsx
@@ -1,13 +1,17 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuth } from "./AuthContext";
 import LoadingSpinner from "../components/LoadingSpinner";
 
 const RequireAuth = ({ allowedRoles }) => {
   const { user, loading } = useAuth();
+  const location = useLocation();
   if (loading) return <LoadingSpinner />;
   if (!user) return <Navigate to="/" replace />;
   if (!allowedRoles.includes(user.role)) {
-    return <Navigate to="/unauthorized" replace />;
+    return <Navigate to="/" replace />;
+  }
+  if (user.profile && user.profile.isUpdated === false && location.pathname !== "/update-profile") {
+    return <Navigate to="/update-profile" replace />;
   }
 
   return <Outlet />;
