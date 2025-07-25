d20d7423f38e48016f541612a7fc2d14deedb15c
TienBV <buivutien1508@gmail.com>
2025-07-25 11:21:11 +0700

    Update NavBar.jsx

--- frontend/src/components/Layouts/NavBar.jsx
+++ frontend/src/components/Layouts/NavBar.jsx
@@ -34,18 +34,11 @@ export default function NavBar() {
         </div>
 
         <div className="flex items-center gap-6">
-          <div className="relative">
-            <Search className="absolute left-2 top-2.5 text-gray-400 w-4 h-4" />
-            <input
-              type="text"
-              placeholder="Tìm kiếm..."
-              className="pl-8 pr-4 py-2 border border-gray-300 rounded-md text-sm w-64 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-            />
-          </div>
+          
 
           {user ? (
             <>
-              <Bell className="w-6 h-6 text-gray-600 cursor-pointer hover:text-indigo-600 transition" />
+              
               <div className="relative" ref={dropdownRef}>
                 <div
                   onClick={() => setShowDropdown(!showDropdown)}
